ca128d7ec8c470640296ec2a08f43108469d13c8
dileep koppula <nanikoppula9494@gmail.com>
2023-03-28 17:37:11 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'engro-assesment';
+  string1: String = '';
+  string2: String = '';
+  anagram: String = '';
+
+  ngOnInit() {}
+
+  IsAnagram() {
+    let a = this.string1;
+    let b = this.string2;
+    console.log(a, b);
+
+    let str1 = a.split('').sort().join('');
+    let str2 = b.split('').sort().join('');
+    if (str1 === str2) {
+      console.log('True');
+      this.anagram = 'TRUE';
+    } else {
+      console.log('False');
+      this.anagram = 'FALSE';
+    }
+  }
 }
